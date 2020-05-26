8ff857308b8bf678b627c5647cd328d7881c6ec7
danst3in <50937807+danst3in@users.noreply.github.com>
2020-05-25 21:10:31 -0700

    Modify folder structure , enable 2 bundles. Bundle for main process and bundle for renderer (react app). Renamed some files for clarity. Main process is now swell.js, while bundles files will be labeled main or renderer.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -8,63 +8,142 @@ const nodeExternals = require("webpack-node-externals");
 // need to be added to this array so webpack will pick them up
 const defaultInclude = path.resolve(__dirname, "src");
 
-module.exports = {
-  mode: "development",
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        loader: "babel-loader",
-        include: defaultInclude,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/,
-        use: [{ loader: "file-loader?name=img/[name]__[hash:base64:5].[ext]" }],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|mp3)$/,
-        use: [
-          { loader: "file-loader?name=font/[name]__[hash:base64:5].[ext]" },
-        ],
-      },
+module.exports = [
+  {
+    mode: "development",
+    entry: path.join(__dirname, "main", "swell.js"),
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "main-bundle.js",
+    },
+    target: "electron-main",
+    node: {
+      __dirname: false,
+      __filename: false,
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          loader: "babel-loader",
+          include: defaultInclude,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.css$/,
+          use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+        },
+        {
+          test: /\.(jpe?g|png|gif)$/,
+          use: [
+            { loader: "file-loader?name=img/[name]__[hash:base64:5].[ext]" },
+          ],
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|mp3)$/,
+          use: [
+            { loader: "file-loader?name=font/[name]__[hash:base64:5].[ext]" },
+          ],
+        },
+      ],
+    },
+    externals: [nodeExternals()],
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify("development"),
+      }),
     ],
-  },
-  target: "electron-renderer",
-  externals: [nodeExternals()],
-  plugins: [
-    new HtmlWebpackPlugin({ title: "Swell v0.4.0" }),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("development"),
-    }),
-  ],
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
-    stats: {
-      colors: true,
-      chunks: false,
-      children: false,
-    },
-    before() {
-      spawn("electron", ["."], {
-        shell: true,
-        env: process.env,
-        stdio: "inherit",
-      })
-        .on("close", (code) => process.exit(0))
-        .on("error", (spawnError) => console.error(spawnError));
+    devtool: "inline-source-map",
+    devServer: {
+      contentBase: path.resolve(__dirname, "dist"),
+      stats: {
+        colors: true,
+        chunks: false,
+        children: false,
+      },
+      before() {
+        spawn("electron", ["."], {
+          shell: true,
+          env: process.env,
+          stdio: "inherit",
+        })
+          .on("close", (code) => process.exit(0))
+          .on("error", (spawnError) => console.error(spawnError));
+      },
     },
   },
-};
+  {
+    mode: "development",
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "renderer-bundle.js",
+    },
+    target: "electron-renderer",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          loader: "babel-loader",
+          include: defaultInclude,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.css$/,
+          use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+        },
+        {
+          test: /\.(jpe?g|png|gif)$/,
+          use: [
+            { loader: "file-loader?name=img/[name]__[hash:base64:5].[ext]" },
+          ],
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|mp3)$/,
+          use: [
+            { loader: "file-loader?name=font/[name]__[hash:base64:5].[ext]" },
+          ],
+        },
+      ],
+    },
+    externals: [nodeExternals()],
+    plugins: [
+      new HtmlWebpackPlugin({ title: "Swell v0.4.0" }),
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify("development"),
+      }),
+    ],
+    devtool: "inline-source-map",
+    devServer: {
+      contentBase: path.resolve(__dirname, "dist"),
+      stats: {
+        colors: true,
+        chunks: false,
+        children: false,
+      },
+      before() {
+        spawn("electron", ["."], {
+          shell: true,
+          env: process.env,
+          stdio: "inherit",
+        })
+          .on("close", (code) => process.exit(0))
+          .on("error", (spawnError) => console.error(spawnError));
+      },
+    },
+  },
+];
